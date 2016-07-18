1936243461fd40cf16ad299fcbd1a396957362d7
Slawa <me@me.com>
2016-07-18 14:19:51 +0200

    Remove controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'statis_pages/home'
-
-  get 'statis_pages/help'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
