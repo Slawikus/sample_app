1984eaa34ac0f5fafb0e75285969b556b5c98ece
Slawa <me@me.com>
2016-07-28 13:51:44 +0200

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:slawa)
-    @non_admin = users(:micky)
+    @user =           users(:slawa)
+    @non_admin =      users(:micky)
+    @non_activated =  users(:lana)
   end
 
   test "index including pagination" do
@@ -11,7 +12,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
+    User.where(activated: true).paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
@@ -21,7 +22,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @user
@@ -37,6 +38,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index as non-admin without non-active users" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: @non_activated.name, count: 0
+    get user_path(@non_activated)
+    assert_redirected_to root_url
   end
 
 end
